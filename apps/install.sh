1e9dc0ab3eb6e9772f2ba218fe3bb7f40c0311a6
Rashad Ansari <rashad.ansari1996@gmail.com>
2023-06-15 00:52:02 +0200

    Update install.sh apps

--- apps/install.sh
+++ apps/install.sh
@@ -8,8 +8,8 @@ brew install --cask google-chrome
 brew install --cask visual-studio-code
 brew install --cask telegram
 # brew install --cask outline-client
-brew install --cask spotify
+# brew install --cask spotify
 brew install --cask docker
 brew install --cask postman
-brew install --cask bitwarden
+# brew install --cask bitwarden
 brew install --cask mounty
