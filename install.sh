24abfeaa8d7fa84cc527c5e57f76e973b0343364
Rashad Ansari <rashad.ansari1996@gmail.com>
2024-01-08 00:47:39 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,9 +10,9 @@ source dev/tools/install.sh
 source dev/git/install.sh
 source dev/vim/install.sh
 source dev/ssh/install.sh
-source dev/go/install.sh
-source dev/python/install.sh
-source dev/node.js/install.sh
+# source dev/go/install.sh
+# source dev/python/install.sh
+# source dev/node.js/install.sh
 # source dev/php/install.sh
 # source dev/java/install.sh
 # source dev/scala/install.sh
