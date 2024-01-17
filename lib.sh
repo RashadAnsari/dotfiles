aee4faa7653a4ce8b87239ec2bf23d4106a85e5a
Rashad Ansari <rashad.ansari1996@gmail.com>
2024-01-17 14:50:16 +0100

    Update lib.sh

--- lib.sh
+++ lib.sh
@@ -1,5 +1,20 @@
 #!/bin/zsh
 
+function install_xcode_tools {
+    if ! xcode-select -p &>/dev/null; then
+        echo "Xcode Command Line Tools are not installed. Installing now..."
+        xcode-select --install
+        echo "Waiting for the installation to complete..."
+        while true; do
+            if xcode-select -p &>/dev/null; then
+                echo "Installation completed."
+                break
+            fi
+            sleep 10
+        done
+    fi
+}
+
 function copyZshEnv {
     mkdir -p $HOME/.zshenvs
     rm -rf $HOME/.zshenvs/$1
