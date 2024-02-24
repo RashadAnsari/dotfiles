6c6cc46fe3e856b4ed57a12ccf99c0a17bff8317
Rashad Ansari <rashad.ansari1996@gmail.com>
2024-02-24 12:42:59 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,14 +6,13 @@ install_xcode_tools
 ./dev/zsh/install.sh
 
 source dev/brew/install.sh
-source dev/nix/install.sh
 source dev/tools/install.sh
 source dev/git/install.sh
 source dev/vim/install.sh
 source dev/ssh/install.sh
-source dev/go/install.sh
 
 # It would be better to use `nix-shell` instead of a global installation.
+# source dev/go/install.sh
 # source dev/python/install.sh
 # source dev/node.js/install.sh
 # source dev/php/install.sh
@@ -21,3 +20,4 @@ source dev/go/install.sh
 # source dev/scala/install.sh
 
 source apps/install.sh
+source dev/nix/install.sh
