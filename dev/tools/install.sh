c9a03357f9d417d31f7779fb8d749e9dac8782b5
Rashad Ansari <rashad.ansari1996@gmail.com>
2024-06-16 15:26:40 +0200

    Update dev/tools/install.sh

--- dev/tools/install.sh
+++ dev/tools/install.sh
@@ -8,26 +8,26 @@ brew install yq
 brew install htop
 brew install telnet
 brew install cmake
-brew install kubectx
-brew install jsonnet
-brew install ansible
-brew tap hashicorp/tap
-brew install terragrunt
+# brew install kubectx
+# brew install jsonnet
+# brew install ansible
+# brew tap hashicorp/tap
+# brew install terragrunt
 # brew install tfenv
-brew install infracost
-brew install awscli
-brew install mongosh
+# brew install infracost
+# brew install awscli
+# brew install mongosh
 
 brew install jump
 echo 'eval "$(jump shell)"' >> $HOME/.zshrc
-brew install helm
-echo "source <(helm completion zsh)" >> $HOME/.zshrc
-brew install stern
-echo "source <(stern --completion=zsh)" >> $HOME/.zshrc
-brew install kubectl
-echo "source <(kubectl completion zsh)" >> $HOME/.zshrc
-brew install --cask google-cloud-sdk
-echo 'source "$(brew --prefix)/share/google-cloud-sdk/path.zsh.inc"' >> $HOME/.zshrc
-echo 'source "$(brew --prefix)/share/google-cloud-sdk/completion.zsh.inc"' >> $HOME/.zshrc
+# brew install helm
+# echo "source <(helm completion zsh)" >> $HOME/.zshrc
+# brew install stern
+# echo "source <(stern --completion=zsh)" >> $HOME/.zshrc
+# brew install kubectl
+# echo "source <(kubectl completion zsh)" >> $HOME/.zshrc
+# brew install --cask google-cloud-sdk
+# echo 'source "$(brew --prefix)/share/google-cloud-sdk/path.zsh.inc"' >> $HOME/.zshrc
+# echo 'source "$(brew --prefix)/share/google-cloud-sdk/completion.zsh.inc"' >> $HOME/.zshrc
 
 copyZshEnv tools
