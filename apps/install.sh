219865220d8a907a953e6a68702c1b4c9b9d7daf
Rashad Ansari <rashad.ansari1996@gmail.com>
2024-06-16 15:12:21 +0200

    Update apps/install.sh

--- apps/install.sh
+++ apps/install.sh
@@ -8,11 +8,11 @@ brew install --cask google-chrome
 brew install --cask visual-studio-code
 brew install --cask telegram
 brew install --cask whatsapp
-# brew install --cask outline-client
+brew install --cask adguard
 brew install --cask spotify
 brew install --cask docker
 brew install --cask postman
-# brew install --cask bitwarden
 brew install --cask slack
 brew install --cask zoom
+# brew install --cask bitwarden
 # brew install --cask hp-smart
