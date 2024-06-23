3dcad373be73e248d2c8033fde65efc17dd4c6c2
Rashad Ansari <rashad.ansari1996@gmail.com>
2024-06-23 13:43:21 +0200

    Update apps/install.sh

--- apps/install.sh
+++ apps/install.sh
@@ -4,15 +4,9 @@ brew install --cask hyper
 rm -rf $HOME/.hyper.js
 cp ./apps/.hyper.js $HOME/.hyper.js
 
-brew install --cask google-chrome
 brew install --cask visual-studio-code
 brew install --cask telegram
-brew install --cask whatsapp
 brew install --cask spotify
 brew install --cask docker
-brew install --cask postman
-brew install --cask slack
-brew install --cask zoom
 # brew install --cask adguard
-# brew install --cask bitwarden
 # brew install --cask hp-smart
