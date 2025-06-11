c573507f79881bf64ea8e944111e364a5ea1030b
Rashad Ansari <rashad.ansari1996@gmail.com>
2025-06-11 12:04:12 +0200

    Update apps/install.sh

--- apps/install.sh
+++ apps/install.sh
@@ -13,7 +13,7 @@ brew install --cask spotify
 # Coding
 brew install --cask visual-studio-code
 brew install --cask cursor
-brew install --cask trae
+brew install --cask void
 brew install --cask docker
 
 # Work
