ab9cf4f911e5c2e75645cbae1897b5bf393ffb8a
Rashad Ansari <rashad.ansari1996@gmail.com>
2025-09-17 21:01:41 +0200

    Update java/install.sh

--- dev/java/install.sh
+++ dev/java/install.sh
@@ -2,5 +2,6 @@
 
 brew install openjdk@21
 ln -sfn $(brew --prefix)/opt/openjdk@21/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-21.jdk
+brew install --cask intellij-idea-ce
 
 copyZshEnv java
