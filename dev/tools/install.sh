54cbc1f0e8d1125ae8698e327f986f3a77b14f64
Rashad Ansari <rashad.ansari1996@gmail.com>
2025-09-18 15:03:29 +0200

    Update tools/install.sh

--- dev/tools/install.sh
+++ dev/tools/install.sh
@@ -7,24 +7,10 @@ brew install yq
 brew install htop
 brew install telnet
 brew install cmake
-# brew install kubectx
-# brew install jsonnet
-# brew install ansible
-# brew install awscli
+brew install gemini-cli
 
 brew install jump
 echo 'eval "$(jump shell)"' >> $HOME/.zshrc
-# brew install helm
-# echo "source <(helm completion zsh)" >> $HOME/.zshrc
-# brew install stern
-# echo "source <(stern --completion=zsh)" >> $HOME/.zshrc
-# brew install kubectl
-# echo "source <(kubectl completion zsh)" >> $HOME/.zshrc
-# brew install --cask google-cloud-sdk
-# echo 'source "$(brew --prefix)/share/google-cloud-sdk/path.zsh.inc"' >> $HOME/.zshrc
-# echo 'source "$(brew --prefix)/share/google-cloud-sdk/completion.zsh.inc"' >> $HOME/.zshrc
 
 # Cursor CLI
 curl https://cursor.com/install -fsS | bash
-
-# copyZshEnv tools
