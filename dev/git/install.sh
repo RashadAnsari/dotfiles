9ebc7085d370aa2c9eac28cc510a0d947fe89823
Rashad Ansari <rashad.ansari1996@gmail.com>
2026-03-30 16:17:42 +0200

    Update git configuration and ignore files
    Remove existing .gitignore and copy new one. Set global git ignore file.

--- dev/git/install.sh
+++ dev/git/install.sh
@@ -3,4 +3,8 @@
 rm -rf $HOME/.gitconfig
 cp ./dev/git/.gitconfig $HOME/.gitconfig
 
+rm -rf $HOME/.gitignore
+cp ./dev/git/.gitignore $HOME/.gitignore
+git config --global core.excludesfile $HOME/.gitignore
+
 copyZshEnv git
