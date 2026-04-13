9f067860f1545c85374044763a6df5218ad12711
Rashad Ansari <rashad.ansari1996@gmail.com>
2026-04-13 13:44:59 +0200

    Delete dev/java directory

--- dev/java/.zshenv
+++
@@ -1,3 +0,0 @@
-export JAVA_HOME=$BREW_PREFIX/opt/openjdk@21
-export PATH="$BREW_PREFIX/opt/openjdk@21/bin:$PATH"
-export CPPFLAGS="-I$BREW_PREFIX/opt/openjdk@21/include $CPPFLAGS"
--- dev/java/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-brew install openjdk@21
-ln -sfn $(brew --prefix)/opt/openjdk@21/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-21.jdk
-brew install --cask intellij-idea-ce
-
-copyZshEnv java
